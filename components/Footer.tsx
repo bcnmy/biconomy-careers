baed0c8cb229e9684ec3a2dfe6484ab02d09e756
Asheer Rizvi <asheer.vil@gmail.com>
2022-03-24 12:13:47 +0530

    feat: footer component.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,55 @@
+import Image from 'next/image';
+
+import biconomyLogo from '../assets/images/biconomy-logo.svg';
+import twitterLogo from '../assets/images/twitter-logo.svg';
+import telegramLogo from '../assets/images/telegram-logo.svg';
+import discordLogo from '../assets/images/discord-logo.svg';
+
 function Footer() {
-  return <div>Hello World!</div>;
+  return (
+    <footer className="h-[18.75rem] w-full bg-bico-gray-300">
+      <div className="container relative mx-auto flex h-full items-center justify-center">
+        <div className="absolute left-0">
+          <a href="https://biconomy.io">
+            <Image src={biconomyLogo} alt="Biconomy" />
+          </a>
+        </div>
+
+        <nav>
+          <ul className="flex items-center">
+            <li className="mr-14 text-2xl font-bold text-white">
+              <a href="https://medium.com/biconomy">Blog</a>
+            </li>
+            <li className="mr-14 text-2xl font-bold text-white">
+              <a href="https://docs.biconomy.io/comm/contact-us">Contact</a>
+            </li>
+            <li className="mr-14 text-2xl font-bold text-white">
+              <a href="https://twitter.com/biconomy">
+                <Image src={twitterLogo} alt="Twitter" />
+              </a>
+            </li>
+            <li className="mr-14 text-2xl font-bold text-white">
+              <a href="https://t.me/biconomy">
+                <Image src={telegramLogo} alt="Telegram" />
+              </a>
+            </li>
+            <li className="text-2xl font-bold text-white">
+              <a href="https://discord.com/invite/HKHxgyEExQ">
+                <Image src={discordLogo} alt="Discord" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <a
+          href="https://biconomy.io"
+          className="absolute right-0 text-2xl font-bold text-white"
+        >
+          © 2022 // Biconomy.io
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Culture from '../components/Culture';
+import Footer from '../components/Footer';
 
 import Header from '../components/Header';
 import Hero from '../components/Hero';
@@ -30,18 +31,7 @@ const Home: NextPage = () => {
         <OpenRoles />
       </main>
 
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+      <Footer />
     </div>
   );
 };
